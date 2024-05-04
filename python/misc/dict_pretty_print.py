fae1518314925e35218b596870f7d3d7fa481e7d
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-05-04 22:22:22 +0200

    feat(dict_pretty_print.py): finished function

---
+++ python/misc/dict_pretty_print.py
@@ -0,0 +1,12 @@
+"""
+TAGS: dict|pretty|pretty print|print
+
+Defines function pd(), which prints a dictionary in an aesthetic readable way using json.dumps()
+"""
+
+import json
+
+
+def pd(dct: dict) -> None:
+    """Prints given dictionary in an aesthetic readable way using json.dumps()"""
+    print(json.dumps(dct, indent=4, default=str))
