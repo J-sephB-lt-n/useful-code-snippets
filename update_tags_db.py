c28806948bb6d7480b587e52c9065b9a0e0ee261
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-05-06 23:07:27 +0200

    feat(update_tags_db.py-tags_db.pickle): finished script

---
+++ update_tags_db.py
@@ -0,0 +1,26 @@
+"""
+Collects all tags from the top of all of the code scripts
+in all folders and subfolders of /snippets/ and 
+saves them in a searchable format in `tags_db.pickle`
+"""
+
+import os
+import pickle
+
+tags_db: dict[str, dict] = {}
+
+for dirpath, dirnames, filenames in os.walk("snippets"):
+    for filename in filenames:
+        filepath = f"{dirpath}/{filename}"
+        print(f"processing {filepath}")
+        with open(filepath, "r", encoding="utf-8") as file:
+            for line_num, line in enumerate(file.readlines()):
+                if line_num == 1:
+                    tags_db[filepath] = {"tags": set(line.replace("TAGS: ", "").rstrip().split("|"))}
+                elif line_num == 2:
+                    tags_db[filepath]["description"] = line.replace("DESCRIPTION: ","").rstrip()
+
+print("processed all files")
+with open("tags_db.pickle", "wb") as file:
+    pickle.dump(tags_db, file, pickle.HIGHEST_PROTOCOL)
+
