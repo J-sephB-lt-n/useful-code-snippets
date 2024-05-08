f3c89e848a67f07d13c8bf5a9a6c54ad1884f24b
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-05-08 21:51:35 +0200

    feat(setup_python_on_ec2_virtual_machine.sh): working (tested on E2 small)

---
+++ snippets/bash/gcp/setup_python_on_ec2_virtual_machine.sh
@@ -0,0 +1,32 @@
+<<'###BLOCK-COMMENT'
+TAGS: cloud|compute engine|ec2|gcp|google|google cloud|install|pyenv|python|setup|set up|virtual machine|vm
+DESCRIPTION: Bash code for setting up python on a google cloud platform EC2 compute engine virtual machine (using pyenv)
+###BLOCK-COMMENT
+
+# TLDR: use pyenv (follow their setup instructions)
+
+sudo apt update
+sudo apt install curl git-all -y
+sudo apt install build-essential libssl-dev zlib1g-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev curl \
+    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
+
+sudo curl https://pyenv.run | bash
+
+echo '
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"' | tee -a ~/.profile  ~/.bashrc
+
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
+
+exec "$SHELL" # restart shell
+
+pyenv install --list | grep " 3\.12"
+pyenv install -v 3.12.3
+pyenv versions
+pyenv global 3.12.3
+pip install --upgrade pip
+
+
