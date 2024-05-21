11a1621545d80d333f7533b01462c60f2e1bfe2a
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-05-21 10:00:46 +0200

    refactor(dict_pretty_print.py): changed function name to ppd()

--- snippets/python/misc/dict_pretty_print.py
+++ snippets/python/misc/dict_pretty_print.py
@@ -1,11 +1,11 @@
 """
 TAGS: dict|pretty|pretty print|print
-DESCRIPTION: Function pd() prints a dictionary in an aesthetic readable way using json.dumps()
+DESCRIPTION: Function ppd() prints a dictionary in an aesthetic readable way using json.dumps()
 """
 
 import json
 
 
-def pd(dct: dict) -> None:
+def ppd(dct: dict) -> None:
     """Prints given dictionary in an aesthetic readable way using json.dumps()"""
     print(json.dumps(dct, indent=4, default=str))
