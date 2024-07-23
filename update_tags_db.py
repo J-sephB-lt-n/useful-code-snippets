51641f89a3954f10e30c2596f4749359417d6910
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-07-23 22:34:55 +0200

    feat(update_tags_db.py): now works for languages without multi-line comments

--- update_tags_db.py
+++ update_tags_db.py
@@ -6,6 +6,7 @@ saves them in a searchable format in `tags_db.pickle`
 
 import os
 import pickle
+import re
 
 tags_db: dict[str, dict] = {}
 
@@ -16,11 +17,21 @@ for dirpath, dirnames, filenames in os.walk("snippets"):
         with open(filepath, "r", encoding="utf-8") as file:
             for line_num, line in enumerate(file.readlines()):
                 if line_num == 1:
-                    tags_db[filepath] = {"tags": set(line.replace("TAGS: ", "").rstrip().split("|"))}
+                    tags_db[filepath] = {
+                        # "tags": set(line.replace("TAGS: ", "").rstrip().split("|"))
+                        "tags": set(
+                            re.search(r"TAGS: (.*)", line)
+                            .groups(1)[0]
+                            .strip()
+                            .split("|")
+                        )
+                    }
                 elif line_num == 2:
-                    tags_db[filepath]["description"] = line.replace("DESCRIPTION: ","").rstrip()
+                    # tags_db[filepath]["description"] = line.replace("DESCRIPTION: ", "").rstrip()
+                    tags_db[filepath]["description"] = (
+                        re.search("DESCRIPTION: (.*)", line).groups(1)[0].strip()
+                    )
 
 print("processed all files")
 with open("tags_db.pickle", "wb") as file:
     pickle.dump(tags_db, file, pickle.HIGHEST_PROTOCOL)
-
